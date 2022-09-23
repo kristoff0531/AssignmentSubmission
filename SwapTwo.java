b79862e6a53cc9934f8c266a645859c6a42ca27e
Kunal Kumar Roy <kroy66194@gmail.com>
2022-09-23 21:58:23 +0530

    Day-1 Assignment Submitted

---
+++ SwapTwo.java
@@ -0,0 +1,14 @@
+public class SwapTwo {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int temp=0;
+        System.out.println("Before swapping a="+a);
+        System.out.println("Before swapping b="+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swappig a= "+a);
+        System.out.println("After swapping b="+b);
+    }
+}
