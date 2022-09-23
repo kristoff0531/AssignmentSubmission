14ab0fe5cb7785609a6e9bd88286a403a3bee35c
Kunal Kumar Roy <kroy66194@gmail.com>
2022-09-23 21:48:29 +0530

    Update SumTillN.java

--- SumTillN.java
+++ SumTillN.java
@@ -1,6 +1,6 @@
 public class SumTillN {
     public static void main(String[] args) {
-        int n=100;
+        int n=50;
         int sum=0;
         for(int i=0;i<=n;i++){
             sum=sum+i;
